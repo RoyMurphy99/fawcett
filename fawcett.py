07b29222781b6f1cb657cbb25886fe9e6a9634e6
RoyMurphy99 <roymurphy99@hotmail.co.uk>
2019-07-16 21:43:59 +0100

    add step 2 to generate a list of candidate place names from the source text

--- fawcett.py
+++ fawcett.py
@@ -28,3 +28,51 @@ m.drawmapboundary()
 
 plt.title('Fawcett\'s travels')
 plt.show()
+
+#
+# step 2 is to extract a list of candidate place names from the text
+#
+
+import re
+regex = re.compile('[^a-zA-Z\s\n]')
+
+os.environ["PROJ_LIB"] = "C:\\Users\\Analysis\\Anaconda3\\pkgs\\proj4-5.2.0-h6538335_1003\\Library\\share"
+
+os.chdir('C:\\Users\\Roy Standard\\Documents\\Python_programming\\fawcett')
+import string
+
+try:
+    maketrans = ''.maketrans
+except AttributeError:
+    # fallback for Python 2
+    from string import maketrans
+
+fawcett = open("fawcett.txt", encoding="utf-8")
+all_words = {}
+
+# the english3.txt file is a free text file of valid english words that I found on the internet
+# there are many others, and I have lost the link for this file. 
+with open("english3.txt") as word_file:
+    english_words = set(word.strip().lower() for word in word_file)
+
+def is_english_word(word):
+    return word.lower() in english_words
+
+table = maketrans({key: None for key in string.punctuation})   
+
+count = 0
+for line in fawcett:
+    count = count + 1
+    line = line.rstrip()
+    line = regex.sub(' ', line)
+#    line = line.translate(table)
+    line=line.lower()
+    line_words=line.split()
+    for word in line_words:
+        if not(is_english_word(word)):
+            if word not in all_words:
+                all_words[word] = 1
+            else:
+                all_words[word] += 1
+                
+candidates = [x.title() for x in all_words.keys()]
